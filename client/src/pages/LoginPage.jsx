8e6b76d66fc8ecd9754d850b728c45553f5bafbd
JithuPrabhakar <join.jitz@gmail.com>
2024-04-01 17:37:47 +0530

    login page started

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,12 @@
 export const LoginPage = () => {
-  return <div>LoginPage</div>
+  return (
+    <div className='mt-4'>
+      <h2 className='text-4xl text-center'>Login</h2>
+      <form className='max-w-md mx-auto border'>
+        <input type='email' placeholder='your@email.com' />
+        <input type='password' placeholder='your password' />
+        <button>Login</button>
+      </form>
+    </div>
+  )
 }
