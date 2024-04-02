adbfff070f0e9d9d2edc488d263f703025f684bc
JithuPrabhakar <join.jitz@gmail.com>
2024-04-02 17:23:41 +0530

    mongo connection

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
+
+app.use(express.json())
 
 app.use(
   cors({
     credentials: true,
     origin: 'http://localhost:5173',
   })
+)
+
+mongoose.connect(
+  'mongodb+srv://jp1234:jp1234@samplecluster.k1g075f.mongodb.net/travel-assistance'
 )
 
 app.get('/test', (req, res) => {
