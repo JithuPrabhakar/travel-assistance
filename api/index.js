096f3cdee0070f57c82e65582cd311c3b41ad02b
JithuPrabhakar <join.jitz@gmail.com>
2024-04-04 14:14:12 +0530

    context starting

--- api/index.js
+++ api/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+require('dotenv').config()
+
+const User = require('./models/User.js')
+
+const bcryptSalt = bcrypt.genSaltSync(10)
+const jwtSecret = '12345'
 
 app.use(express.json())
 
@@ -12,17 +21,47 @@ app.use(
   })
 )
 
-mongoose.connect(
-  'mongodb+srv://jp1234:jp1234@samplecluster.k1g075f.mongodb.net/travel-assistance'
-)
+mongoose.connect(process.env.MONGO_URL)
 
 app.get('/test', (req, res) => {
   res.json('test ok')
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { name, email, password } = req.body
-  res.json({ name, email, password })
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    })
+    res.json({ createdUser: user })
+  } catch (e) {
+    res.status(422).json(e)
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+  if (user) {
+    const checkPassword = bcrypt.compareSync(password, user.password)
+    if (checkPassword) {
+      jwt.sign(
+        { email: user.email, id: user._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err
+          res.cookie('token', token).json('pass ok')
+        }
+      )
+    } else {
+      res.status(422).json('pass not ok')
+    }
+  } else {
+    res.json('user not found')
+  }
 })
 
 app.listen(8080)
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,44 @@
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+import { Link, Navigate } from 'react-router-dom'
 
 export const LoginPage = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [redirect, setRedirect] = useState(false)
+
+  async function handleLogin(e) {
+    e.preventDefault()
+    try {
+      axios.post('/login', { email, password }, { withCredentials: true })
+      alert('Login successful')
+      setRedirect(true)
+    } catch (e) {
+      alert('Login failed')
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to='/' />
+  }
+
   return (
     <div className='mt-4 grow flex items-center justify-around'>
       <div className='mb-64'>
         <h2 className='text-4xl text-center mb-4'>Login</h2>
-        <form className='max-w-md mx-auto'>
-          <input type='email' placeholder='your@email.com' />
-          <input type='password' placeholder='your password' />
+        <form className='max-w-md mx-auto' onSubmit={handleLogin}>
+          <input
+            type='email'
+            placeholder='your@email.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type='password'
+            placeholder='your password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className='primary'>Login</button>
           <div className='text-center py-2 text-gray-500'>
             Don&apos;t have an account yet?{' '}
